d517c1e43b5ea74280bfeab0b6df1607bb90e26e
anastasia-chaplits <anastasia.chaplits@contractors.medallia.com>
2021-08-11 11:52:12 +0300

    Add new cell

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,19 @@
+import './add-cell.css';
+import { useActions } from '../hooks/use-actions';
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useActions();
+
+  return (
+    <h1>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    </h1>
+  );
+};
+
+export default AddCell;
